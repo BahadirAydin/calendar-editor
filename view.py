0f27b4f32dd7db72a98313141cc273a0636d0578
BahadirAydin <bahadiraydin1923@gmail.com>
2023-11-22 11:23:25 +0300

    naming

--- view.py
+++ view.py
@@ -4,22 +4,22 @@ class View:
         self.schedules = {}  # Stores schedules with their respective filters
         self.current_view = None  # To keep track of the main view for notifications
 
-    def add_schedule(self, schedule_id, schedule):
+    def addSchedule(self, schedule_id, schedule):
         self.schedules[schedule_id] = {'schedule': schedule, 'filters': {}}
 
-    def delete_schedule(self, schedule_id):
+    def deleteSchedule(self, schedule_id):
         if schedule_id in self.schedules:
             del self.schedules[schedule_id]
 
-    def set_filter(self, schedule_id, **kwargs):
+    def setFilter(self, schedule_id, **kwargs):
         if schedule_id in self.schedules:
             self.schedules[schedule_id]['filters'] = kwargs
 
-    def remove_filters(self, schedule_id):
+    def removefilters(self, schedule_id):
         if schedule_id in self.schedules:
             self.schedules[schedule_id]['filters'] = {}
 
-    def list_items(self):
+    def listItems(self):
         for schedule_id, data in self.schedules.items():
             filters = data['filters']
             for event in data['schedule'].events:
@@ -32,17 +32,17 @@ class View:
                 return False
         return True
 
-    def attach_view(self, view_id):
+    def attachView(self, view_id):
         self.current_view = view_id
 
-    def detach_view(self, view_id):
+    def detachView(self, view_id):
         if self.current_view == view_id:
             self.current_view = None
 
     @classmethod
-    def list_views(cls, user):
+    def listViews(cls, user):
         return cls.views.get(user.id, [])
 
     @classmethod
-    def get_view(cls, view_id):
+    def getView(cls, view_id):
         return cls.views.get(view_id)
