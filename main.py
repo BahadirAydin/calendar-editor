088080b3a6e44dbacb0f5e2bea850f3a30033593
deniz <denizcan_yilmaz@hotmail.com>
2023-11-22 11:47:02 +0300

    main.py to be modified

--- main.py
+++ main.py
@@ -1,83 +1,89 @@
 from event import Event
 from schedule import Schedule
 from user import User
-import uuid
+from view import View
 
-# Temporary storage for demonstration purposes
-events = []
 users = []
 schedules = []
 current_user = None
 
-def create_event():
-    print("\nCreate Event")
-    event_type = input("Enter event type (e.g., MEETING, SEMINAR): ")
-    start = input("Enter start time (YYYY-MM-DD-HH:MM): ")
-    end = input("Enter end time (YYYY-MM-DD-HH:MM): ")
-    period = input("Enter period: ")
-    description = input("Enter description: ")
-    location = input("Enter location: ")
-    protection = input("Enter protection level: ")
-    assignee = input("Enter assignee: ")
-
-    event = Event(event_type, start, end, period, description, location, protection, assignee)
-    events.append(event)
-    print(f"Event created with ID: {event.id}")
-
-def view_event():
-    event_id = input("Enter event ID to view: ")
-    for event in events:
-        if str(event.id) == event_id:
-            print(vars(event))
-            return
-    print("Event not found.")
-
-def update_event():
-    event_id = input("Enter event ID to update: ")
-    for event in events:
-        if str(event.id) == event_id:
-            # Update event logic goes here
-            print(f"Event {event_id} updated.")
-            return
-    print("Event not found.")
-
-def delete_event():
-    event_id = input("Enter event ID to delete: ")
-    global events
-    events = [event for event in events if str(event.id) != event_id]
-    print(f"Event {event_id} deleted.")
+def create_user():
+    username = input("Enter username: ")
+    email = input("Enter email: ")
+    fullname = input("Enter fullname: ")
+    passwd = input("Enter password: ")
+    user = User(username, email, fullname, passwd)
+    users.append(user)
+    print(f"User created with ID: {user.id}")
 
 def switch_user():
     global current_user
     user_id = input("Enter user ID to switch to: ")
     for user in users:
-        if str(user.id) == user_id:
+        if user.id == user_id:
             current_user = user
             print(f"Switched to user: {current_user.username}")
             return
     print("User not found.")
 
+def create_event():
+    if not current_user:
+        print("Please switch to a user first.")
+        return
+    event_type = input("Enter event type: ")
+    start = input("Enter start time: ")
+    end = input("Enter end time: ")
+    period = input("Enter period: ")
+    description = input("Enter description: ")
+    location = input("Enter location: ")
+    protection = input("Enter protection level: ")
+    assignee = input("Enter assignee: ")
+    event = Event(event_type, start, end, period, description, location, protection, assignee)
+    print(f"Event created with ID: {event.id}")
+    return event
+
+def create_schedule():
+    if not current_user:
+        print("Please switch to a user first.")
+        return
+    description = input("Enter schedule description: ")
+    protection = input("Enter protection level: ")
+    schedule = Schedule(description, protection)
+    schedules.append(schedule)
+    print(f"Schedule created with ID: {schedule.id}")
+
+def add_event_to_schedule():
+    schedule_id = input("Enter the schedule ID to add the event to: ")
+    schedule = next((s for s in schedules if s.id == str(schedule_id).strip()), None)
+    if schedule:
+        event = create_event()
+        if event:
+            schedule.add_event(event)
+            print("Event added to the schedule.")
+    else:
+        print("Schedule not found.")
+
 def main_menu():
     while True:
-        print("\n--- Calendar/Schedule Editor ---")
-        print("1. Create Event")
-        print("2. View Event")
-        print("3. Update Event")
-        print("4. Delete Event")
-        print("5. Switch User")
+        print("\n--- Schedule Manager ---")
+        print("1. Create User")
+        print("2. Switch User")
+        print("3. Create Event")
+        print("4. Create Schedule")
+        print("5. Add Event to Schedule")
         print("6. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            create_event()
+            create_user()
         elif choice == "2":
-            view_event()
-        elif choice == "3":
-            update_event()
-        elif choice == "4":
-            delete_event()
-        elif choice == "5":
             switch_user()
+        elif choice == "3":
+            create_event()
+        elif choice == "4":
+            create_schedule()
+        elif choice == "5":
+            add_event_to_schedule()
         elif choice == "6":
             print("Exiting the application.")
             break
