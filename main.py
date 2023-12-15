fe0310c1834b8510fb50fdf66ef62d47d05b49ca
BahadirAydin <bahadiraydin1923@gmail.com>
2023-12-15 23:12:38 +0300

    color

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ from handler import *
 from view_handlers import *
 import atexit
 import shlex
-import sqlite3
 from retrieve_objects import retrieve_objects
+from colorama import Fore, Style
 
 schedule_manager = ScheduleManager()
 
-VALID_COMMANDS = ["adduser", "signin"]
-HELP_TEXT = """Valid commands are:
-adduser <username> <email> <fullname> <password>
-deleteuser <username> <password>
-signin <username> <password>
-addschedule <username> <description> <protection>
-deleteschedule <username> <schedule_id>
-addevent <username> <schedule_id> <event_description> <event_type> <start> <end> <period> <description> <location> <protection> <assignee>
-deleteevent <username> <schedule_id> <event_id>
-changepassword <username> <password> <new_password>
-updateevent <username> <schedule_id> <event_description> <event_type> <start> <end> <period> <description> <location> <protection> <assignee>
-createview <description>
-attachview <view_name> <description>
-detachview <view_name> <description>
-addtoview <view_name> <schedule_name>
-PRINTUSER <username>
-PRINTSCHEDULE <username> <schedule_id>
+HELP_TEXT = f"""{Fore.GREEN}Valid commands are:{Style.RESET_ALL}
+{Fore.CYAN}adduser {Fore.WHITE}<username> <email> <fullname> <password>{Style.RESET_ALL}
+{Fore.CYAN}deleteuser {Fore.WHITE}<username> <password>{Style.RESET_ALL}
+{Fore.CYAN}signin {Fore.WHITE}<username> <password>{Style.RESET_ALL}
+{Fore.CYAN}addschedule {Fore.WHITE}<username> <description> <protection>{Style.RESET_ALL}
+{Fore.CYAN}deleteschedule {Fore.WHITE}<username> <schedule_id>{Style.RESET_ALL}
+{Fore.CYAN}addevent {Fore.WHITE}<username> <schedule_id> <event_description> <event_type> <start> <end> <period> <description> <location> <protection> <assignee>{Style.RESET_ALL}
+{Fore.CYAN}deleteevent {Fore.WHITE}<username> <schedule_id> <event_id>{Style.RESET_ALL}
+{Fore.CYAN}changepassword {Fore.WHITE}<username> <password> <new_password>{Style.RESET_ALL}
+{Fore.CYAN}updateevent {Fore.WHITE}<username> <schedule_id> <event_description> <event_type> <start> <end> <period> <description> <location> <protection> <assignee>{Style.RESET_ALL}
+{Fore.CYAN}createview {Fore.WHITE}<description>{Style.RESET_ALL}
+{Fore.CYAN}attachview {Fore.WHITE}<view_name> <description>{Style.RESET_ALL}
+{Fore.CYAN}detachview {Fore.WHITE}<view_name> <description>{Style.RESET_ALL}
+{Fore.CYAN}addtoview {Fore.WHITE}<view_name> <schedule_name>{Style.RESET_ALL}
+{Fore.CYAN}PRINTUSER {Fore.WHITE}<username>{Style.RESET_ALL}
+{Fore.CYAN}PRINTSCHEDULE {Fore.WHITE}<username> <schedule_id>{Style.RESET_ALL}
+{Fore.CYAN}PRINTVIEW {Fore.WHITE}<view_name>{Style.RESET_ALL}
 """
 
 
